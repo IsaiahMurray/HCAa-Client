7289b19839f89a404fb9ff537fdef5e88a54309d
Isaiah Murray <murray.isaiah13@gmail.com>
2021-07-31 23:40:52 -0400

    updated file name

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Signup from "./Signup";
+import Signup from "./Register";
 import Login from "./Login";
 import { Grid, Paper, Button } from "@material-ui/core";
 
